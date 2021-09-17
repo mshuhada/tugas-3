2754c9d1d6a677a4ad7d36b4f209e7366969f109
mshuhada <aatshuada@gmail.com>
2021-09-17 23:52:32 +0700

    tugas looping angka

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+
+public class Main {
+    public static void main(String[] args) {
+
+        for (int i=3; i<=35; i+=6) {
+            System.out.println(i);
+        }
+        System.out.println("--------");
+
+        int i=3;
+        do {
+            System.out.println(i);
+            i+=6;
+        } while (i <= 35);
+
+        System.out.println("--------");
+
+        int n = 3;
+        while (n <= 35) {
+           System.out.println(n);
+           n+=6;
+
+        }
+        System.out.println("--------");
+
+
+    }
+}
